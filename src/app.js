66239e353cd0aea955ce3fb775940a584225dbd7
theaton89 <taylornheaton@gmail.com>
2021-04-16 17:13:37 -0500

    started formatDate function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return `${day}${hours}${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +17,8 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let conditionElement = document.querySelector("#condition");
   conditionElement.innerHTML = response.data.weather[0].main;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5ec00814f3c8665ba79cad93cf79765f";
